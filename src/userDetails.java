4eb32b92fb523bbf6cf227066af29f46a8b6c1a0
NMSanka <nmsankapadma@gmail.com>
2017-12-13 14:10:18 +0530

    now

---
+++ src/userDetails.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class userDetails extends JFrame {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton button1;
+    private JButton button2;
+}
